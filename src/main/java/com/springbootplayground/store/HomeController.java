4b1e58a166bc679c3789cfbaffd29fbdbaf39974
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-17 13:53:10 +0900

    add HomeController and index.html for initial web page setup

---
+++ src/main/java/com/springbootplayground/store/HomeController.java
@@ -0,0 +1,12 @@
+package com.springbootplayground.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+  @RequestMapping("/")
+  public String index() {
+    return "index.html";
+  }
+}

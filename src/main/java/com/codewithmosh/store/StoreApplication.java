f11549ff63d9616ec1b4e016c302a6f9268b83a5
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-23 13:40:51 +0900

    Remove unused classes and interfaces related to payment and user management

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -3,7 +3,6 @@ package com.codewithmosh.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
@@ -11,5 +10,4 @@ public class StoreApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
     }
-
 }

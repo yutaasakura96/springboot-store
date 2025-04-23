667886c77f9635b570fdc30edb070217d858808d
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-23 16:16:08 +0900

    Add Lombok dependency and refactor User entity to use Lombok annotations

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,13 +1,13 @@
 package com.example.store;
 
-import org.springframework.boot.SpringApplication;
+import com.example.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        User.builder().name("John").password("password").email("john@example.com").build();
     }
 }
--- src/main/java/com/example/store/entities/User.java
+++ src/main/java/com/example/store/entities/User.java
@@ -6,7 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
@@ -23,35 +29,4 @@ public class User {
     @Column(nullable = false, name = "password")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }

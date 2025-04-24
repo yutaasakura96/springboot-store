55442da1c2c86b20f04aa5d8817f1c1225a755d3
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-24 13:13:05 +0900

    Refactor StoreApplication: remove commented-out code and streamline main method; update application.yaml to remove unused payment and mail configurations

--- src/main/java/com/store/store/StoreApplication.java
+++ src/main/java/com/store/store/StoreApplication.java
@@ -3,30 +3,12 @@ package com.store.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import com.store.store.entities.Address;
-import com.store.store.entities.Profile;
-import com.store.store.entities.Tag;
-import com.store.store.entities.User;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var user = User.builder()
-                .name("John")
-                .password("password")
-                .email("john@codewithmosh.com")
-                .build();
-
-        var profile = Profile.builder()
-                        .bio("bio")
-                        .build();
-
-        user.setProfile(profile);
-
-        System.out.println(user);
+       ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
     }
 
 }

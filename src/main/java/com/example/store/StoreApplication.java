21949c0fd72b146f5ca12fca75365dd98a250870
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-24 14:24:13 +0900

    Implement user deletion in StoreApplication and ensure SQL logging is enabled in application.yaml

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.example.store.repositories.UserRepository;
+
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+       var repository = context.getBean("userRepository", UserRepository.class);
+       repository.deleteById(1L);
     }
 
 }

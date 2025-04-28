863014bc2387c8b231e577934e3fbf9691b2a192
yuta asakura <yuta.asakura.se@gmail.com>
2025-04-28 16:25:03 +0900

    Add pagination and sorting for product retrieval
    Introduced methods in UserService to fetch products with pagination and sorting options. Updated the application entry point to demonstrate paginated product retrieval. Simplified import statements in modified files.

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.math.BigDecimal;
-
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
         var service =  context.getBean(UserService.class);
-        service.fetchProductsBySpecifications("prod", BigDecimal.valueOf(1), null);
+        service.fetchPaginatedProducts(0,10);
     }
 }
--- src/main/java/com/example/store/services/UserService.java
+++ src/main/java/com/example/store/services/UserService.java
@@ -8,8 +8,7 @@ import com.example.store.specifications.ProductSpec;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -131,6 +130,27 @@ public class UserService {
             System.out.println(u);
             u.getAddresses().forEach(System.out::println);
         });
+    }
+
+    public void fetchSortedProducts() {
+        var sort = Sort.by("name").and(
+                Sort.by("price").descending()
+        );
+
+        productRepository.findAll(sort).forEach(System.out::println);
+    }
+
+    public void fetchPaginatedProducts(int pageNumber, int size) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, size);
+        Page<Product> page = productRepository.findAll(pageRequest);
+
+        var products = page.getContent();
+        products.forEach(System.out::println);
+
+        var totalPages = page.getTotalPages();
+        var totalElements = page.getTotalElements();
+        System.out.println("Total Pages: " + totalPages);
+        System.out.println("Total Elements: " + totalElements);
     }
 
     @Transactional

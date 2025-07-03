a8b07eaaa4cbcc5b14ce42c45793492a73bef614
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-03 16:28:08 +0900

    Enhance UserController to support sorting in getAllUsers endpoint
    Updated the getAllUsers method to accept an optional sorting parameter, allowing users to sort results by name or email. Default sorting is set to name if an invalid parameter is provided. Removed the unused profile field from the User entity to streamline the codebase.

--- src/main/java/com/example/store/controllers/UserController.java
+++ src/main/java/com/example/store/controllers/UserController.java
@@ -4,11 +4,11 @@ import com.example.store.dtos.UserDto;
 import com.example.store.mappers.UserMapper;
 import com.example.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -18,8 +18,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+          @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
+    ) {
+        if (!Set.of("name", "email").contains(sortBy))
+            sortBy = "name";
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
--- src/main/java/com/example/store/entities/User.java
+++ src/main/java/com/example/store/entities/User.java
@@ -44,9 +44,6 @@ public class User {
         address.setUser(null);
     }
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private Profile profile;
-
     @ManyToMany
     @JoinTable(
         name = "wishlist",

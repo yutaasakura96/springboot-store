8d2545a8d1a0e596dfca0542ff44bd68924d2164
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-04 16:05:04 +0900

    Add change password functionality in UserController and introduce ChangePasswordRequest DTO
    Implemented a new POST endpoint in UserController for changing user passwords, which verifies the old password before updating to a new one. Added ChangePasswordRequest DTO to encapsulate the password change data, enhancing user security and management capabilities.

--- src/main/java/com/example/store/controllers/UserController.java
+++ src/main/java/com/example/store/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.example.store.controllers;
 
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import com.example.store.dtos.ChangePasswordRequest;
 import com.example.store.dtos.RegisterUserRequest;
 import com.example.store.dtos.UpdateUserRequest;
 import com.example.store.dtos.UserDto;
 import com.example.store.mappers.UserMapper;
 import com.example.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.Set;
 
 @RestController
@@ -77,6 +80,24 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
         userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable Long id,
+            @RequestBody ChangePasswordRequest request
+    ) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (!user.getPassword().equals(request.getOldPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/example/store/dtos/ChangePasswordRequest.java
@@ -0,0 +1,9 @@
+package com.example.store.dtos;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+}

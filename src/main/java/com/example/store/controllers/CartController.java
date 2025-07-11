f04e4a0bc812cf37fe1ee86b0e9643e4f421ffd2
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-11 22:25:37 +0900

    Add OpenAPI documentation support to CartController
    Added the springdoc-openapi-starter-webmvc-ui dependency to the project for OpenAPI documentation. Enhanced CartController with Swagger annotations to document the API endpoints, including a summary for the addToCart method and a tag for the controller. These changes improve API visibility and facilitate better integration with documentation tools.

--- src/main/java/com/example/store/controllers/CartController.java
+++ src/main/java/com/example/store/controllers/CartController.java
@@ -7,6 +7,9 @@ import com.example.store.dtos.UpdateCartItemRequest;
 import com.example.store.exceptions.CartNotFoundException;
 import com.example.store.exceptions.ProductNotFoundException;
 import com.example.store.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,7 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/carts")
+@Tag(name = "carts")
 public class CartController {
     private final CartService cartService;
 
@@ -33,7 +37,9 @@ public class CartController {
     }
 
     @PostMapping("/{cartId}/items")
+    @Operation(summary = "Adds a product to the cart.")
     public ResponseEntity<CartItemDto> addToCart(
+            @Parameter(description = "The ID of the cart.")
             @PathVariable UUID cartId,
             @RequestBody AddItemToCartRequest request
     ) {

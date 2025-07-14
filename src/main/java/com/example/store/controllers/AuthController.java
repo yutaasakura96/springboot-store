db0ee8855e5f5a393f852896547ccd9d3bee34a4
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-14 21:11:12 +0900

    Add JWT authentication support and update dependencies
    Introduced JwtService for generating JWT tokens and updated AuthController to return a JwtResponse upon successful login. Added necessary JWT dependencies in pom.xml to support token generation. This enhances the application's authentication mechanism by implementing token-based authentication for user sessions.

--- src/main/java/com/example/store/controllers/AuthController.java
+++ src/main/java/com/example/store/controllers/AuthController.java
@@ -1,7 +1,8 @@
 package com.example.store.controllers;
 
+import com.example.store.dtos.JwtResponse;
 import com.example.store.dtos.LoginRequest;
-import com.example.store.repositories.UserRepository;
+import com.example.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,19 +10,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
@@ -29,7 +29,9 @@ public class AuthController {
                 )
         );
 
-        return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
---
+++ src/main/java/com/example/store/services/JwtService.java
@@ -0,0 +1,22 @@
+package com.example.store.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    public String generateToken(String email) {
+        final long tokenExpiration = 86400;
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
+                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .compact();
+
+    }
+}

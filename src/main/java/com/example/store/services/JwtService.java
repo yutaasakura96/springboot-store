94d55ad84c26bdb016b4cb682fcb388d30aa501f
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-14 21:25:47 +0900

    Add JWT secret configuration and update dependencies
    Created a new .env.example file for environment variable configuration. Updated pom.xml to include the spring-dotenv dependency for loading environment variables. Modified JwtService to use the JWT secret from the application configuration, enhancing security by allowing dynamic secret management. Updated application.yaml to reference the JWT secret variable. These changes improve the flexibility and security of JWT token generation.

--- src/main/java/com/example/store/services/JwtService.java
+++ src/main/java/com/example/store/services/JwtService.java
@@ -2,12 +2,16 @@ package com.example.store.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400;
 
@@ -15,7 +19,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
 
     }

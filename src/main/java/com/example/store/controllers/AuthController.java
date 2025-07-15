0cdd6889ee017de952c71b59476e4ab27f0b1955
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-15 18:38:47 +0900

    Add logging filter and enhance token validation logging
    Introduced a new LoggingFilter to log incoming request URIs and outgoing response statuses. Additionally, added a log statement in the validate method of AuthController to indicate when the validation is called. These changes improve observability and debugging capabilities in the authentication process.

--- src/main/java/com/example/store/controllers/AuthController.java
+++ src/main/java/com/example/store/controllers/AuthController.java
@@ -36,6 +36,8 @@ public class AuthController {
 
     @PostMapping("/validate")
     public boolean validate(@RequestHeader("Authorization") String authHeader) {
+        System.out.println("Validate called");
+
         var token = authHeader.replace("Bearer ", "");
 
         return jwtService.validateToken(token);

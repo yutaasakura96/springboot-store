8fc9dbd4268fe60807d88b5043df46e31d21dc71
yuta asakura <yuta.asakura.se@gmail.com>
2025-07-15 18:56:22 +0900

    Implement JWT authentication filter and update security configuration
    Added JwtAuthenticationFilter to handle JWT token validation in incoming requests. Updated SecurityConfig to include the new filter in the security chain, enhancing the authentication process. Refactored JwtService to improve token validation and added a method to extract the email from the token. These changes strengthen the security framework by ensuring proper authentication handling with JWTs.

--- src/main/java/com/example/store/config/SecurityConfig.java
+++ src/main/java/com/example/store/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.store.config;
 
+import com.example.store.filters.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +17,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,9 +53,10 @@ public class SecurityConfig {
                         .requestMatchers("/carts/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
--- src/main/java/com/example/store/services/JwtService.java
+++ src/main/java/com/example/store/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.example.store.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -27,18 +28,23 @@ public class JwtService {
 
     public boolean validateToken(String token) {
         try {
-            var claims = Jwts.parser()
-                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload();
+            var claims = getClaims(token);
 
             return claims.getExpiration().after(new Date());
-        }
-        catch (JwtException ex) {
+        } catch (JwtException ex) {
             return false;
         }
+    }
 
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
     }
 }
